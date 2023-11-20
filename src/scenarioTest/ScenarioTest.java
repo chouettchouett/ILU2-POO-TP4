6de72de01df85990cca80143ff60280ab85553b4
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2023-11-20 20:32:25 +0100

    Update ScenarioTest.java

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -1,14 +1,19 @@
 package scenarioTest;
 
+import produit.Poisson;
 import produit.Produit;
-import vilagegauloisold.Etal;
-import vilagegauloisold.IEtal;
+import produit.Sanglier;
+import villagegaulois.Etal;
+import villagegaulois.IEtal;
 
 public class ScenarioTest {
 	public static void main(String[] args) {
-		IEtal[] etals = new IEtal[3];
-		Etal<Sanglier> etalSanglier = new Etal<>();
-		etals[0] = etalSanglier;
+		IEtal<Produit>[] marche = new IEtal[3];
+		IEtal<Sanglier> etalS = new Etal<>();
+		IEtal<Poisson> etalP = new Etal<>();
+		marche[0] = etalS;
+		marche[1] = etalP;
+				
 	}
 	
 }
