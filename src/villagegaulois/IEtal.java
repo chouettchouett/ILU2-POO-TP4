34822edb9f89bcff2545eb0c775525f83eed28c1
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2023-11-20 20:30:36 +0100

    Update IEtal.java

--- src/villagegaulois/IEtal.java
+++ src/villagegaulois/IEtal.java
@@ -1,11 +1,34 @@
 package villagegaulois;
 
 import personnages.Gaulois;
+import produit.Produit;
 
-public interface IEtal  {
-	private Gaulois getVendeur();
-	double donnerPrix();
-	int contientProduit(String produit, int quantiteSouhaitee);
-	double acheterProduit(int quantiteSouhaitee);
-	String etatEtal();
+public interface IEtal<P> {
+
+	boolean isEtalOccupe();
+
+	Gaulois getVendeur();
+
+	int getQuantite();
+
+	P getProduit();
+
+	void occuperEtal(Gaulois vendeur, P produit, int quantite);
+
+	boolean contientProduit(String produit);
+
+	int acheterProduit(int quantiteAcheter);
+
+	void libererEtal();
+
+	/**
+	 * 
+	 * @param produit
+	 * @return donneesVente est un tableau de chaine contenant [0] : un boolean
+	 *         indiquant si l'étal est occupé [1] : nom du vendeur [2] : produit
+	 *         vendu [2] : quantité de produit à vendre au début du marché [4] :
+	 *         quantité de produit vendu
+	 */
+	String[] etatEtal();
+
 }
